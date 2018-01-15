6eff2c4076568b28cdf1c1802b53d4956ca53ab4
zzzhou <ziye123zoe@gmail.com>
2018-01-15 16:46:11 +0200

    testCase

--- xjoin/src/tjFast/testCase.java
+++ xjoin/src/tjFast/testCase.java
@@ -50,14 +50,27 @@ public class testCase {
         testCase1.runTest(xml_query_file, xml_document_file, rdb_table);
     }
 
+    public void naiveTestCase3() throws Exception{
+        //naive test case 1: PC
+        String xml_query_file = "xjoin/src/testCaseDataSet/testCase1/xml_query_file.xml";
+        String xml_document_file = "xjoin/src/testCaseDataSet/testCase1/xml_document_file.xml";
+        String rdb_table = "xjoin/src/testCaseDataSet/testCase1/rdb_table";
+        queryAnalysis testCase1 = new queryAnalysis();
+        testCase1.runTest(xml_query_file, xml_document_file, rdb_table);
+    }
+
 
 
     static public void main(String[] args) throws Exception {
         testCase t = new testCase();
-        //test case 1
+        //test case 1:PC
         t.xjoinTestCase1();
-//        t.naiveTestCase1();
-
-
+        t.naiveTestCase1();
+        //test case 2:AD-1
+        t.xjoinTestCase1();
+        t.naiveTestCase1();
+        //test case 3:AD-2
+        t.xjoinTestCase1();
+        t.naiveTestCase1();
     }
 }
